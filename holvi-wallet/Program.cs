fab644f0d5eb51d7afb6d6ea71a3804f5af967e8
De-Crypted <email@example.com>
2022-09-15 15:49:03 +0300

    Added holvi-wallet project

---
+++ holvi-wallet/Program.cs
@@ -0,0 +1,18 @@
+﻿using holviwallet;
+using holviwallet.Data;
+using BlazorDesktop.Hosting;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = BlazorDesktopHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Window.UseTitle("Holvi Wallet");
+
+builder.Services.AddSingleton<WeatherForecastService>();
+
+if (builder.HostEnvironment.IsDevelopment())
+{
+    builder.UseDeveloperTools();
+}
+
+await builder.Build().RunAsync();
