ba3807a1b5a8af0810921a84efae3b4a392c6088
Johan August Sandels <johan.august@sandels.org>
2022-12-18 12:23:21 +0200

    multi

--- package.sh
+++ package.sh
@@ -15,42 +15,85 @@ _on_error() {
 }
 trap '_on_error $?' ERR
 
-export COMPONENT=$1
-export VARIANT=$2
-export DIST=$3
+export VARIANT=$1
+export DIST=$2
+export COMPONENTS=${*:3}
+
+if [[ "$COMPONENTS" = "" ]]
+then
+  COMPONENTS="daemon miner wallet kryolite"
+fi
 
 export GITHUB_REPOSITORY=${GITHUB_REPOSITORY:-kryolite-crypto}
 export DOCKER_BUILDKIT=1
 
-id=$(docker create "ghcr.io/${GITHUB_REPOSITORY}/builder:${COMPONENT}-${VARIANT}")
+declare -A pids
+for COMPONENT in $COMPONENTS; do
+  (
+    id=$(docker create "ghcr.io/${GITHUB_REPOSITORY}/builder:${COMPONENT}-${VARIANT}")
 
-output=$(mktemp -d)
+    output=$(mktemp -d)
 
-case "$VARIANT" in
-  win-*)
-    case "$COMPONENT" in
-      kryolite)
-        docker cp "$id:/usr/local/bin/kryolite.exe" "$output"
+    case "$VARIANT" in
+      win-*)
+        case "$COMPONENT" in
+          kryolite)
+            docker cp "$id:/build/kryolite.exe" "$output"
+          ;;
+          *)
+            docker cp "$id:/build/${COMPONENT}.exe" "$output/kryolite-${COMPONENT}.exe"
+          ;;
+        esac
       ;;
       *)
-        docker cp "$id:/usr/local/bin/kryolite-${COMPONENT}.exe" "$output"
+        case "$COMPONENT" in
+          kryolite)
+            docker cp "$id:/usr/local/bin/kryolite" "$output"
+          ;;
+          *)
+            docker cp "$id:/usr/local/bin/kryolite-${COMPONENT}" "$output"
+          ;;
+        esac
       ;;
     esac
-  ;;
-  *)
-    case "$COMPONENT" in
-      kryolite)
-        docker cp "$id:/usr/local/bin/kryolite" "$output"
-      ;;
-      *)
-        docker cp "$id:/usr/local/bin/kryolite-${COMPONENT}" "$output"
-      ;;
-    esac
-  ;;
-esac
 
-docker rm "$id"
+    docker rm "$id"
 
-zip -jpr "${DIST}/kryolite-${COMPONENT}-${VARIANT}.zip" "$output"
+    zip -jpr "${DIST}/kryolite-${COMPONENT}-${VARIANT}.zip" "$output"
+  ) 2>&1 | sed -le "s#^#${COMPONENT}: #;" &
+  pids[$COMPONENT]=$!
+done
 
-echo "PACKAGE OK"
+declare -A statuses
+for COMPONENT in "${!pids[@]}"; do
+  pid=${pids[$COMPONENT]}
+
+  trap '' ERR
+  set +e
+    wait "$pid"
+    code=$?
+  set -e
+  trap '_on_error $?' ERR
+
+  if [[ "$code" -eq 0 ]]; then
+    statuses[$COMPONENT]=ok
+  else
+    statuses[$COMPONENT]=fail
+  fi
+done
+
+failed=no
+for COMPONENT in "${!statuses[@]}"; do
+  status=${statuses[$COMPONENT]}
+  echo "component $COMPONENT package ${status}"
+
+  [[ "$status" == "fail" ]] && failed=yes
+done
+
+if [[ "$failed" == "yes" ]]
+then
+  echo "PACKAGE FAILED"
+  exit 1
+else
+  echo "PACKAGE OK"
+fi
