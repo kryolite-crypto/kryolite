11e70b68f98fe59f1b5138e6b77c3d9a23204d5b
De-Crypted <email@example.com>
2022-12-30 14:20:44 +0200

    Added --throttle option to miner. Converted miner to use System.Commandline

--- miner/Program.cs
+++ miner/Program.cs
@@ -2,79 +2,93 @@
 using System.Security.Cryptography;
 using System.Text;
 using Kryolite.Shared;
-using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
-
-IConfiguration configuration = new ConfigurationBuilder()
-    .AddCommandLine(args)
-    .Build();
+using System.CommandLine;
 
 Blocktemplate current = new Blocktemplate();
 var tokenSource = new CancellationTokenSource();
 
-while (true) {
-    var httpClient = new HttpClient();
+var rootCmd = new RootCommand("Kryolite Miner");
 
-    var request = await httpClient.GetAsync($"{configuration["url"]}/blocktemplate?wallet={configuration["address"]}");
+var nodeOption = new Option<string>(name: "--url", description: "Node url", getDefaultValue: () => "http://localhost:5000");
+rootCmd.AddGlobalOption(nodeOption);
 
-    request.EnsureSuccessStatusCode();
+var walletOption = new Option<string>(name: "--address", description: "Wallet address");
+rootCmd.AddGlobalOption(walletOption);
 
-    var json = await request.Content.ReadAsStringAsync();
-    var blocktemplate = JsonConvert.DeserializeObject<Blocktemplate>(json);
+var throttleOption = new Option<int?>(name: "--throttle", description: "Milliseconds to sleep between hashes");
+rootCmd.AddGlobalOption(throttleOption);
 
-    if (blocktemplate == null || blocktemplate.ParentHash == current.ParentHash) {
-        Thread.Sleep(TimeSpan.FromSeconds(1));
-        continue;
-    }
+rootCmd.SetHandler(async (url, address, throttle) => { 
+    while (true) {
+        var httpClient = new HttpClient();
 
-    current = blocktemplate;
+        var request = await httpClient.GetAsync($"{url}/blocktemplate?wallet={address}");
 
-    Console.WriteLine($"{DateTime.Now}: New job {blocktemplate.Height}, diff = {BigInteger.Log(blocktemplate.Difficulty.ToWork(), 2)}");
+        request.EnsureSuccessStatusCode();
 
-    tokenSource.Cancel();
-    tokenSource = new CancellationTokenSource();
-    
-    new Thread(async () => {
-        var token = tokenSource.Token;
+        var json = await request.Content.ReadAsStringAsync();
+        var blocktemplate = JsonConvert.DeserializeObject<Blocktemplate>(json);
 
-        using var sha256 = SHA256.Create();
-        Random rd = new Random();
-
-        var nonce = new byte[32];
-        var concat = new Concat
-        {
-            Buffer = new byte[64]
-        };
-
-        Array.Copy(current.Nonce, 0, concat.Buffer, 0, 32);
-
-        var target = current.Difficulty.ToTarget();
-
-        while (!token.IsCancellationRequested) {
-            rd.NextBytes(nonce);
-            Array.Copy(nonce, 0, concat.Buffer, 32, 32);
-
-            var sha256Hash = (SHA256Hash)sha256.ComputeHash(concat.Buffer);
-
-            var result = sha256Hash.ToBigInteger();
-
-            if (result.CompareTo(target) <= 0) {
-                Console.WriteLine($"{DateTime.Now}: Block found");
-                var solution = current;
-                var bytes = new byte[32];
-                Array.Copy(concat.Buffer, 32, bytes, 0, 32);
-
-                solution.Solution = bytes;
-
-                var json = JsonConvert.SerializeObject(blocktemplate);
- 
-                using var content = new StringContent(json, Encoding.UTF8, "application/json");
-                await httpClient.PostAsync($"{configuration["url"]}/solution", content);
-
-                break;
-            }
+        if (blocktemplate == null || blocktemplate.ParentHash == current.ParentHash) {
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            continue;
         }
-    }).UnsafeStart();
 
-    Thread.Sleep(TimeSpan.FromSeconds(1));
-}
+        current = blocktemplate;
+
+        Console.WriteLine($"{DateTime.Now}: New job {blocktemplate.Height}, diff = {BigInteger.Log(blocktemplate.Difficulty.ToWork(), 2)}");
+
+        tokenSource.Cancel();
+        tokenSource = new CancellationTokenSource();
+        
+        new Thread(async () => {
+            var token = tokenSource.Token;
+
+            using var sha256 = SHA256.Create();
+            Random rd = new Random();
+
+            var nonce = new byte[32];
+            var concat = new Concat
+            {
+                Buffer = new byte[64]
+            };
+
+            Array.Copy(current.Nonce, 0, concat.Buffer, 0, 32);
+
+            var target = current.Difficulty.ToTarget();
+
+            while (!token.IsCancellationRequested) {
+                rd.NextBytes(nonce);
+                Array.Copy(nonce, 0, concat.Buffer, 32, 32);
+
+                var sha256Hash = (SHA256Hash)sha256.ComputeHash(concat.Buffer);
+                var result = sha256Hash.ToBigInteger();
+
+                if (result.CompareTo(target) <= 0) {
+                    Console.WriteLine($"{DateTime.Now}: Block found");
+                    var solution = current;
+                    var bytes = new byte[32];
+                    Array.Copy(concat.Buffer, 32, bytes, 0, 32);
+
+                    solution.Solution = bytes;
+
+                    var json = JsonConvert.SerializeObject(blocktemplate);
+    
+                    using var content = new StringContent(json, Encoding.UTF8, "application/json");
+                    await httpClient.PostAsync($"{url}/solution", content);
+
+                    break;
+                }
+
+                if (throttle is not null) {
+                    Thread.Sleep(throttle.Value);
+                }
+            }
+        }).UnsafeStart();
+
+        Thread.Sleep(TimeSpan.FromSeconds(1));
+    }
+}, nodeOption, walletOption, throttleOption);
+
+await rootCmd.InvokeAsync(args);
