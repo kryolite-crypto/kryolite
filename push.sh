811f58205a4fa48f9580eee8e48bf2cef8411774
Johan August Sandels <johan.august@sandels.org>
2023-02-14 18:58:20 +0200

    push.sh

---
+++ push.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+_on_error() {
+  set +x
+
+  trap '' ERR
+  line_path=$(caller)
+  line=${line_path% *}
+  path=${line_path#* }
+
+  echo ""
+  echo "ERR $path:$line $BASH_COMMAND exited with $1"
+  exit 1
+}
+trap '_on_error $?' ERR
+
+ok=no
+for ((i=1;i<=3;i++))
+do
+  echo "push $i / 3 - $1"
+  if docker push "$1"
+  then
+    ok=yes
+    break
+  fi
+
+  sleep 1
+done
+
+if [[ "$ok" == "yes" ]]
+then
+  echo "push OK with $1"
+  exit 0
+else
+  echo "push FAIL with $1"
+  exit 1
+fi
