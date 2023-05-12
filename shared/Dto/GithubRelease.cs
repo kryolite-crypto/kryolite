8b57bd98e70bdc90a8d15e9e86d5c69af56fb2f2
De-Crypted <email@example.com>
2023-05-12 20:58:18 +0300

    Include missing file in commit

---
+++ shared/Dto/GithubRelease.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Kryolite.Shared;
+
+// only partially implemented
+// https://api.github.com/repos/kryolite-crypto/kryolite/releases/latest
+public class GithubRelease
+{
+    [JsonPropertyName("tag_name")]
+    public string TagName { get; set; }
+}
