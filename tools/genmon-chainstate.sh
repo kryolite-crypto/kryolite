3229fa8e2fa2c0d9465675e3c3fed4e30f3e4f97
0xF00 <0xf00@kryolite.io>
2023-09-24 11:06:16 +0300

    genmon script

---
+++ tools/genmon-chainstate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eEuo pipefail
+
+STATE=$(curl -fsL https://testnet-1.kryolite.io/chainstate)
+STATE2=$(curl -fsL http://testnet-2.kryolite.io/chainstate)
+
+HEIGHT=$(echo $STATE | jq ".height")
+WEIGHT=$(echo $STATE | jq ".weight" | tr -d '"')
+BLOCKS=$(echo $STATE | jq ".blocks")
+HEALTH=OK
+
+if [[ $STATE != $STATE2 ]]; then
+    HEALTH=DIVERGED
+    notify-send "Watchman" "Testnet chains have diverged"
+fi
+
+echo "<txt>[Height : "$(($HEIGHT))"] [Weight : "$(($WEIGHT / 1000000))M"] [Blocks : "$(($BLOCKS))"] ["$(echo $HEALTH)"]</txt>"
