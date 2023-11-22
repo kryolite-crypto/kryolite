72256aa2d8afcb9bcdf80f5d514ef9f402266c90
0xF00 <0xf00@kryolite.io>
2023-11-22 10:43:43 +0200

    Wallet mining UI

--- benchmarks/Program.cs
+++ benchmarks/Program.cs
@@ -1,4 +1,14 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System.Diagnostics;
+using BenchmarkDotNet.Running;
 using Kryolite.Benchmarks;
 
-BenchmarkRunner.Run<GrasshopperHash>();
+//BenchmarkRunner.Run<TransactionVerify>();
+
+var tx = new TransactionVerify();
+var sw = Stopwatch.StartNew();;
+for (var i = 0; i < 1_000_000; i++)
+{
+    tx.Verify();
+}
+sw.Stop();
+Console.WriteLine(1_000_000 / sw.Elapsed.TotalSeconds);
