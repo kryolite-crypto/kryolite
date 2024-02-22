a8199c5dd5de54f0510314d15b079972808a5df7
0xF00 <0xf00@kryolite.io>
2024-02-22 22:57:21 +0200

    fixes

--- shared.tests/Blockchain/AddressTests.cs
+++ shared.tests/Blockchain/AddressTests.cs
@@ -9,10 +9,10 @@ namespace Kryolite.Shared.Tests
         [Fact]
         public void AddressEqualityTest()
         {
-            var addr1 = (Address)"kryo:weai6vamhaawc23ijn9x8ju9ry5djyyy43nurcj4jw";
-            var addr2 = (Address)"kryo:weahfksgx68n2vv77tgai3uxatitys2662mwx6ai6a";
-            var addr3 = (Address)"kryo:weai6vamhaawc23ijn9x8ju9ry5djyyy43nurcj4jw";
-            var addr4 = (Address)"kryo:weand2747q6pak2847yzsyx5r365ij58fat4adgnhw";
+            var addr1 = (Address)"kryo:acega4gtcwjfsjz5r9iwxduk62e9jhc5296naygg";
+            var addr2 = (Address)"kryo:aa8e2p2u6ukjigyd9d3323d4sa5q3wc97g3278kj";
+            var addr3 = (Address)"kryo:acega4gtcwjfsjz5r9iwxduk62e9jhc5296naygg";
+            var addr4 = (Address)"kryo:aceekpv4jmegwd7css5ye7k9uinapp9kfef27je3";
 
             Assert.NotEqual(addr1, addr2);
             Assert.Equal(addr1, addr3);
